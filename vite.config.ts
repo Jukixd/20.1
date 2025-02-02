6350073e0b4dde19bb5546b378bc0deeb37dda91
tmadz <tomee.r@email.cz>
2025-02-02 19:11:04 +0100

    fixos

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,5 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/20.1/',
-  plugins: [react()],
-});
+  base: '/20.1/', 
+})
